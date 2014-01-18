73c8a2bae9f6b866304aa06c3302fb6362e53d4c
Steve Shogren <steve.a.shogren@gmail.com>
2014-01-18 15:42:45 -0500

    Exercise 1

---
+++ chapter1.hs
@@ -0,0 +1,34 @@
+-- Exercise 1
+
+Prelude Data.Ratio> 5 + 8
+13
+Prelude Data.Ratio> 3 * 5 + 8
+23
+Prelude Data.Ratio> 2 + 4
+6
+Prelude Data.Ratio> (+) 2 4
+6
+Prelude Data.Ratio> sqrt 16
+4.0
+Prelude Data.Ratio> succ 6
+7
+Prelude Data.Ratio> succ 7
+8
+Prelude Data.Ratio> succ 9
+10
+Prelude Data.Ratio> succ 8
+9
+Prelude Data.Ratio> sin (pi / 2)
+1.0
+Prelude Data.Ratio> truncate pi
+3
+Prelude Data.Ratio> round 3.5
+4
+Prelude Data.Ratio> round 3.4
+3
+Prelude Data.Ratio> floor 3.7
+3
+Prelude Data.Ratio> ceiling 3.3
+4
+
+-- Exercise 2 
