11c6dd26a26e31bf11d82fe2e7029c2e87c212c3
Steve Shogren <steve.a.shogren@gmail.com>
2014-02-10 08:08:06 -0500

    Making type synonyms

--- bookstore.hs
+++ bookstore.hs
@@ -1,5 +1,6 @@
 -- bookstore.hs
 
+-- here we have a type BookInfo, created with the constructor Book 
 data BookInfo = Book Int String [String]
                 deriving (Show)
 
@@ -8,3 +9,18 @@ data MagazineInfo = Magazine Int String [String]
 
 myInfo = Book 098098098 "Programming"
          ["Richard bird", "oege de moor"]
+
+-- The double BookReview here is the type and the constructor
+-- they can and usually should be named the same thing
+data BookReview = BookReview BookInfo CustomerID String
+
+-- These are "type synonyms" that give more readability and
+-- clarity to the actual business concept
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
+
+-- here is a synonym for a verbose type
+-- the Type is a tuple of a BookInfo and a BookReview
+type BookRecord = (BookInfo, BookReview)
